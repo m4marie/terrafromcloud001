7ab2764a15a319940ff2ce1a56010d5d36a4175e
m4marie <m4marie>
2025-08-08 17:57:49 +0200

    terraform test fixed

--- ec2.tf
+++ ec2.tf
@@ -1,19 +1,41 @@
 provider "aws" {
-  region = "us-west-1"
+  region = var.aws_region
 }
- 
-# EC2 instances
-resource "aws_instance" "example" {
-  count         = 3
-  ami           = "ami-0a0409af1cb831414"
-  instance_type = "t2.micro"
- 
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "instance_count" {
+  default = 4
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "Amazon Linux 2 AMI (HVM), SSD Volume Type for us-east-1"
+  default     = "ami-0c02fb55956c7d316" # Update this to your region's AMI if needed
+}
+
+variable "key_name" {
+  description = "Name of an existing EC2 Key Pair"
+  type        = string
+}
+
+resource "aws_instance" "ec2_instances" {
+  count         = var.instance_count
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
   tags = {
-    Name = "MyEC2Instance-${count.index + 1}"
+    Name = "Terraform-EC2-${count.index + 1}"
   }
 }
- 
-# Output the public IPs of the instances
+
 output "instance_public_ips" {
-  value = aws_instance.example[*].public_ip
+  value = aws_instance.ec2_instances[*].public_ip
 }
+
