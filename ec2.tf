eb645fee647a4ed5a6b123505498e5107f1fdcc8
m4marie <m4marie>
2025-08-08 17:06:19 +0200

    second commit terraform test

--- ec2.tf
+++
@@ -1,5 +0,0 @@
-region               = "us-west-1"
-instance_count       = 3
-ami_id               = "ami-0a0409af1cb831414"
-instance_type        = "t2.micro"
-instance_name_prefix = "MyEC2Instance"
